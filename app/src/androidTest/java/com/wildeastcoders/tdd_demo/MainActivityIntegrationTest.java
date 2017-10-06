98e4727c4785c3e144c514b648ffb6a711d26de5
Majfrendmartin <raciborski.p@gmail.com>
2017-10-06 19:48:50 +0200

    first, simple test

---
+++ app/src/androidTest/java/com/wildeastcoders/tdd_demo/MainActivityIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.wildeastcoders.tdd_demo;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Majfrendmartin on 2017-10-05.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityIntegrationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testButtonClickedChangesText() throws Exception {
+        onView(withId(R.id.btnSubmit)).perform(click());
+
+        onView(withId(R.id.tvText)).check(matches(withText("123")));
+    }
+}
--- app/src/main/java/com/wildeastcoders/tdd_demo/MainActivity.java
+++ app/src/main/java/com/wildeastcoders/tdd_demo/MainActivity.java
@@ -1,13 +1,27 @@
 package com.wildeastcoders.tdd_demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvText)
+    TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSubmit)
+    void onSubmitCLicked() {
+        tvText.setText("123");
     }
 }
